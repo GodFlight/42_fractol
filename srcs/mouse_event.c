1afe82adb4baa168907e2d5078e75b782ebdebe7
GodFlight <prokushevmaksim@gmail.com>
2019-08-27 20:31:17 +0300

    little refactoring and mouse coordinate correction

--- srcs/mouse_event.c
+++ srcs/mouse_event.c
@@ -6,13 +6,13 @@
 /*   By: rkeli <rkeli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:07:00 by rkeli             #+#    #+#             */
-/*   Updated: 2019/06/06 22:31:40 by rkeli            ###   ########.fr       */
+/*   Updated: 2019/08/27 16:53:59 by rkeli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void		m_event(int mousecode, t_fractol *fractol)
+static void		mouse_click_event(int mousecode, t_fractol *fractol)
 {
 	if (mousecode == 4 || mousecode == 1)
 	{
@@ -43,7 +43,7 @@ int				mouse_event(int mousecode, int x, int y, void *param)
 		return (0);
 	fractol->event.mouse_move_x = x;
 	fractol->event.mouse_move_y = y;
-	m_event(mousecode, fractol);
+	mouse_click_event(mousecode, fractol);
 	run_cl(fractol);
 	return (0);
 }
@@ -51,14 +51,22 @@ int				mouse_event(int mousecode, int x, int y, void *param)
 int				mouse_move(int x, int y, void *param)
 {
 	t_fractol	*fractol;
+	int			tmp_x;
+	int 		tmp_y;
 
 	fractol = (t_fractol*)param;
 	if (x > WIDTH || y > HEIGHT || x < 0 || y < 0)
 		return (0);
+	tmp_x = x;
+	tmp_y = y;
 	if (fractol->event.stop_jl != 0)
 		return (0);
-	fractol->event.jl_move_x = (double)x * fractol->event.zoom;
-	fractol->event.jl_move_y = (double)y * fractol->event.zoom;
+	if (tmp_x > WIDTH / 2)
+		tmp_x = (WIDTH / 2) - (tmp_x - WIDTH / 2);
+	if (tmp_y > HEIGHT / 2)
+		tmp_y= (HEIGHT / 2) - (tmp_y - HEIGHT / 2);
+	fractol->event.jl_move_x = (double)tmp_x * fractol->event.zoom;
+	fractol->event.jl_move_y = (double)tmp_y * fractol->event.zoom;
 	run_cl(fractol);
 	return (0);
 }
