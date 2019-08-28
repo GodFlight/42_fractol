41ba60459706fdc8201fc8d70fe52350ab0fd8d9
GodFlight <prokushevmaksim@gmail.com>
2019-08-28 03:03:04 +0300

    fractal choose processing added

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rkeli <rkeli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 15:21:44 by rkeli             #+#    #+#             */
-/*   Updated: 2019/08/25 23:30:59 by rkeli            ###   ########.fr       */
+/*   Updated: 2019/08/28 02:57:24 by rkeli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ static void			change_fractol(char *argv, t_fractol *fractol)
 		fractol->flg = 1;
 	else if (!(ft_strcmp(argv, "julia")))
 		fractol->flg = 2;
-	else if (!(ft_strcmp(argv, "carpet")))
+	else if (!(ft_strcmp(argv, "manjulia")))
 		fractol->flg = 3;
+	else if (!(ft_strcmp(argv, "mandjuliaburn")))
+		fractol->flg = 4;
 	else if (!(ft_strcmp(argv, "burningship")))
 	{
-		fractol->flg = 4;
+		fractol->flg = 5;
 		fractol->event.mve_horiz = -1.675537;
 		fractol->event.mve_vertic = -0.034484;
 		fractol->event.zoom = 0.000039;
@@ -31,7 +33,7 @@ static void			change_fractol(char *argv, t_fractol *fractol)
 	else
 	{
 		ft_putstr("usage: ./fractol target_name\n");
-		ft_putstr("target_name: mandelbrot, julia, carpet, burningship\n");
+		ft_putstr("target_name: mandelbrot, julia, manjulia, mandjuliaburn, burningship\n");
 		exit(32);
 	}
 }
@@ -78,16 +80,17 @@ int					main(int argc, char **argv)
 	if (argc == 1 && argv)
 	{
 		ft_putstr("usage: ./fractol target_name\n");
-		ft_putstr("target_name: mandelbrot, julia, carpet, burningship\n");
+		ft_putstr("target_name: mandelbrot, julia, manjulia, mandjuliaburn, " \
+			"burningship\n");
 		exit(32);
 	}
 	if (!(fractol = malloc(sizeof(t_fractol))))
 		exit(30);
+	change_fractol(argv[1], fractol);
 	fractol_init(fractol);
 	fractol->mlx = mlx_init();
 	img_init(fractol);
 	fractol->win = mlx_new_window(fractol->mlx, WIDTH, HEIGHT, "Fractol");
-	change_fractol(argv[1], fractol);
 	create_cl(fractol);
 	run_cl(fractol);
 	mlx_hook(fractol->win, 17, 0, &ft_close, fractol);
